044aaacc432df24e367921d1018be0b1c952ca8c
true-vue <pawelkolodziej81@gmail.com>
2024-03-14 23:16:10 +0100

    Cors disabled for experimenting...

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 // Adding default CORS policy that will all to make a call from browser with any origin (url) with any http method and any http header.
-builder.Services.AddCors(o => o.AddDefaultPolicy(c => c.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+// builder.Services.AddCors(o => o.AddDefaultPolicy(c => c.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
